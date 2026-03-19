db27df6ba9a8472d9a725ace188a2389bb73dcd6
Andres Vergauwen <andres.vergauwen@hotmail.com>
2026-03-19 10:47:52 +0100

    feat: initialize project with Ember and TypeScript

---
+++ types/global.d.ts
@@ -0,0 +1,7 @@
+// Types for compiled templates
+declare module 'frontend-decide-policy-impact-report/templates/*' {
+  import { TemplateFactory } from 'ember-cli-htmlbars';
+
+  const tmpl: TemplateFactory;
+  export default tmpl;
+}
